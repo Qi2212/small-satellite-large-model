d4fd85668d22723fd4bda171d7aef0319c3b9a61
QI2212 <2758486401@qq.com>
2024-10-28 20:55:22 +0800

    hid some information

--- ssl_model/settings.py
+++ ssl_model/settings.py
@@ -9,27 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-$-llfwi93y12l+8zmd1mm-^%##4jw@!fzx&pz#=!&=m@-h3fo%"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ["*"]
-
-
-# Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -72,12 +57,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ssl_model.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
-#数据库连接101.132.177.19
+#add/replace your info here.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -85,13 +65,10 @@ DATABASES = {
         'USER':'',
         'PASSWORD':'',
         'HOST':'',
-        'PORT':'',
+        'PORT':3306,
 
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -108,27 +85,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'zh-Hans'
-
 TIME_ZONE = 'Asia/Shanghai'
-
 USE_I18N = True
-
 USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
 MEDIA_URL = '/userfile/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'userfile')
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -144,7 +108,7 @@ CORS_ALLOW_METHODS = (
     'PUT',
     'VIEW',
 )
-# 发出实际请求时可以使用的非标准HTTP标头的列表。默认为:
+
 CORS_ALLOW_HEADERS = (
     'XMLHttpRequest',
     'X_FILENAME',
